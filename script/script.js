920e36508264eedde65dd618d367fbb9be49a478
ahmdgun0 <ahmdgun0@gmail.com>
2021-11-21 20:55:19 +0200

    hellow world

--- script/script.js
+++ script/script.js
@@ -7,3 +7,21 @@ scrolling.forEach(btn => {
         window.scrollTo({top:elment, behavior:"smooth"})
     });
 });
+
+// if(url.include('code=')){ =>>>>>>> TEST IF TRUE IN FORGET PASSWORD PAGE
+//     const newPassword = `
+//     <h2>Forget Password</h2>
+//     <form autocomplete="off">
+//     <label for="pass">New Password</label>
+//     <input type="password" id="pass" required>
+//     <p class="error-msg"></p>
+
+//     <label for="pass">Repeat Password</label>
+//     <input type="password" id="pass2" required>
+//     <p class="error-msg"></p>
+    
+//     <button type="submit">CONFIRM</button>
+//     </form>
+//     `;
+//     document.querySelector(".forgetpassword .form").innerHTML = newPassword;
+// }
